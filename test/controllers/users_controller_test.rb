6bfb4158631a467b931cefc0f3cbe467040d9d39
Guilherme Freire Hack <gfreirehack@gmail.com>
2014-11-28 21:50:32 -0200

    chapter 12: user following

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
 	def setup
-		@user       = users(:michael)
-		@other_user = users(:archer)
-	end
+	    @user = users(:michael)
+	    @other_user = users(:archer)
+	  end
 
 	test "should get new" do
 		get :new
@@ -57,4 +57,14 @@ class UsersControllerTest < ActionController::TestCase
 		end
 		assert_redirected_to root_url
 	end
+
+	test "should redirect following when not logged in" do
+    get :following, id: @user
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get :followers, id: @user
+    assert_redirected_to login_url
+  end
 end
